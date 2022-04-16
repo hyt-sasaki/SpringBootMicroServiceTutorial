2cba1553a264c512c8cf5039fdd6a99644c83889
Hayato Sasaki <h.sasaki.ynu@gmail.com>
2022-04-16 19:31:12 +0900

    add jooq

--- greeting-infra/src/main/java/com/example/greeting/infra/repository/HelloRepository.java
+++ greeting-infra/src/main/java/com/example/greeting/infra/repository/HelloRepository.java
@@ -2,15 +2,30 @@ package com.example.greeting.infra.repository;
 
 import com.example.greeting.domain.hello.HelloEntity;
 import com.example.greeting.domain.hello.IHelloRepository;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
 
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 
+import static com.example.generated.jooq.sample.Tables.GREETINGS;
+
 @Repository
+@RequiredArgsConstructor
 public class HelloRepository implements IHelloRepository {
+    @NonNull
+    private final DSLContext jooq;
+
     @Override
     public HelloEntity fetch() {
-        return HelloEntity.create("mocked message at " + OffsetDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
+        val message = jooq.select(GREETINGS.MESSAGE)
+                .from(GREETINGS)
+                .where(GREETINGS.ID.eq(1L))
+                .fetchOptional(GREETINGS.MESSAGE)
+                .orElse("mocked message");
+        return HelloEntity.create(message + " at " + OffsetDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
     }
 }
