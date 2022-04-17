4633c11fbac6a134658579b3f9423b16564d5e06
Hayato Sasaki <h.sasaki.ynu@gmail.com>
2022-04-17 20:19:23 +0900

    add application.yml

--- greeting-infra/src/main/java/com/example/greeting/infra/config/GreetingInfraBeanConfig.java
+++ greeting-infra/src/main/java/com/example/greeting/infra/config/GreetingInfraBeanConfig.java
@@ -1,34 +1,9 @@
 package com.example.greeting.infra.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.jooq.DSLContext;
-import org.jooq.SQLDialect;
-import org.jooq.impl.DefaultDSLContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.example.greeting.infra")
 public class GreetingInfraBeanConfig {
-
-    @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-
-        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        config.setJdbcUrl("jdbc:mysql://127.0.0.1:13306/sample");
-        config.setUsername("docker");
-        config.setPassword("docker");
-
-        return new HikariDataSource(config);
-    }
-
-    @Bean
-    public DSLContext dlsContext() {
-        return new DefaultDSLContext(dataSource(), SQLDialect.MYSQL);
-    }
 }
